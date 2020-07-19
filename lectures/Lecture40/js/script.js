da80c90cf3364263f1d39e88b87fd61ece65f89b
burcuh94 <burcuh94@unet.univie.ac.at>
2020-07-19 18:36:22 +0200

    new lecture examples

---
+++ lectures/Lecture40/js/script.js
@@ -0,0 +1,10 @@
+var message = "in global";
+console.log("global: message = " + message)
+
+var a = function(){
+    var message = "inside a";
+    console.log("a: message ="+ message);
+
+}
+
+a();
